8ee9667f07b31fe61f7dec6c580db24a7c2bb3fc
Bennie van der Wel <bennievanderwel@gmail.com>
2019-03-13 16:41:24 +0100

    WK-58 Added all blackspots to the map with thier corresponding markers

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,24 +1,37 @@
 import React from "react";
 
+import L from "leaflet";
 import "leaflet/dist/leaflet";
 import "leaflet/dist/leaflet.css";
 import "amsterdam-amaps/dist/nlmaps/dist/assets/css/nlmaps.css";
 import "amsterdam-stijl/dist/css/ams-stijl.css";
 import amaps from "amsterdam-amaps/dist/amaps";
+
 import { MapContainer } from "./Map.styled";
+import { getAllBlackspots } from "../../services/geo-api";
+import { MarkerTypes } from "./customMarkers";
 
 class Map extends React.Component {
   componentDidMount() {
-    amaps.createMap({
+    const map = amaps.createMap({
       center: {
         latitude: 52.36988741057662,
         longitude: 4.8966407775878915
       },
       layer: "standaard",
       target: "mapdiv",
-      marker: true,
       search: true,
       zoom: 14
+    });
+
+    getAllBlackspots().then(data => {
+      L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+          return L.marker(latlng, {
+            icon: MarkerTypes[feature.properties.spot_type]
+          });
+        }
+      }).addTo(map);
     });
   }
 
