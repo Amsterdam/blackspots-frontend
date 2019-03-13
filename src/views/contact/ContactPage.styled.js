a41e1140b3acea008b7428bf428f78fd0973aab5
Bennie van der Wel <bennievanderwel@gmail.com>
2019-03-13 14:32:44 +0100

    Clean up based on code review

---
+++ src/views/contact/ContactPage.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContactStyled = styled.div`
+  table tr td:nth-child(1) {
+    font-family: ${props => {
+      return props.theme.fontFamily.avenirRoman;
+    }};
+    color: #787878;
+    width: 220px;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 20px;
+  }
+`;
