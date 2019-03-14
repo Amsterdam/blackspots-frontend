b4e8df974e4d98b263d70a897277ae407137ad9d
Bennie van der Wel <bennievanderwel@gmail.com>
2019-03-14 11:31:23 +0100

    WK-63 Added feedback when the server is not available and added a loading spinner when retrieving blackspot data from the server

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -7,11 +7,13 @@ import "amsterdam-amaps/dist/nlmaps/dist/assets/css/nlmaps.css";
 import "amsterdam-stijl/dist/css/ams-stijl.css";
 import amaps from "amsterdam-amaps/dist/amaps";
 
-import { MapContainer } from "./Map.styled";
+import { MapContainer, ErrorDiv, LoadingDiv, Spinner } from "./Map.styled";
 import { getAllBlackspots } from "../../services/geo-api";
 import { MarkerTypes } from "./customMarkers";
 
 class Map extends React.Component {
+  state = { error: false, loading: true };
+
   componentDidMount() {
     const map = amaps.createMap({
       center: {
@@ -24,21 +26,43 @@ class Map extends React.Component {
       zoom: 14
     });
 
-    getAllBlackspots().then(data => {
-      L.geoJSON(data, {
-        pointToLayer: function(feature, latlng) {
-          return L.marker(latlng, {
-            icon: MarkerTypes[feature.properties.spot_type]
-          });
-        }
-      }).addTo(map);
-    });
+    getAllBlackspots()
+      .then(data => {
+        L.geoJSON(data, {
+          pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+              icon: MarkerTypes[feature.properties.spot_type]
+            });
+          }
+        }).addTo(map);
+        this.setState({ loading: false });
+      })
+      .catch(() => {
+        this.setState({ error: true, loading: false });
+      });
   }
 
   render() {
+    const { loading, error } = this.state;
+
     return (
       <MapContainer>
-        <div id="mapdiv" style={{ height: "100%" }} />
+        <div id="mapdiv" style={{ height: "100%" }}>
+          {loading && (
+            <LoadingDiv>
+              <Spinner />
+            </LoadingDiv>
+          )}
+          {error && (
+            <ErrorDiv>
+              <h3>Oops</h3>
+              <p>
+                De server is momenteel niet bereikbaar. Probeer het later nog
+                eens.
+              </p>
+            </ErrorDiv>
+          )}
+        </div>
       </MapContainer>
     );
   }
