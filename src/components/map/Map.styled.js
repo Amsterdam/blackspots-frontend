124d91cdd1016b4edea154580e2b5f6b5035d7b4
Bennie van der Wel <bennievanderwel@gmail.com>
2019-05-02 10:41:53 +0200

    WK-91 style clean up

--- src/components/map/Map.styled.js
+++ src/components/map/Map.styled.js
@@ -1,15 +1,6 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const MapContainer = styled.div`
   height: 100%;
   width: 100%;
-`;
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
 `;
