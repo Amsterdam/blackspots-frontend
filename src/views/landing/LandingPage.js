bd883dd9f7bf06779c751447352c1455f9981ac6
Bennie van der Wel <bennievanderwel@gmail.com>
2019-05-22 14:00:50 +0200

    WK-108 Reworked landing page styling

--- src/views/landing/LandingPage.js
+++ src/views/landing/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import auth from 'shared/auth/auth';
 import styles from './LandingPage.module.scss';
 import logo from 'assets/media/amsterdam-logo.svg';
-import hero from 'assets/media/main-header-hero-default.png';
+import hero from 'assets/media/main-header-hero-default.jpg';
 
 export default () => {
   function login() {
@@ -10,31 +10,38 @@ export default () => {
   }
   return (
     <div className={styles.Container}>
-      <div className={styles.Header}>
-        <img src={logo} className={styles.Logo} alt="Amsterdam logo" />
-        <h1>Werkgroepblackspots</h1>
-      </div>
-      <div className={styles.HeroContainer}>
-        <img alt="Hero" src={hero} className={styles.Hero} />
-      </div>
       <div className={styles.Content}>
-        <h1> Welkom bij Werkgroepblackspots</h1>
-        <p className={styles.Intro}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-        <h2>Inloggen</h2>
-        <button onClick={login} className={styles.LoginBtn}>
-          Datapunt account
-        </button>
-        <p>
-          Account Aanvragen?
-          <br />
-          Neem contact op met{' '}
-          <a href="mailto:e.van.den.bout@amsterdam.nl">Elsbeth van den Bout</a>
-        </p>
+        <div className={styles.Header}>
+          <img src={logo} className={styles.Logo} alt="Amsterdam logo" />
+          <h1>Werkgroepblackspots</h1>
+        </div>
+        <div className={styles.Menu} />
+        <div className={styles.HeroContainer}>
+          <img alt="Hero" src={hero} className={styles.Hero} />
+        </div>
+        <div className={styles.Section}>
+          <h1> Welkom bij Werkgroepblackspots</h1>
+          <p className={styles.Intro}>
+            <b>Welkom bij de Werkgroep Blackspots Amsterdam</b>
+            <br />
+            <br />
+            Via deze pagina kun je inloggen op de kaart van de Werkgroep
+            Blackspots Amsterdam (WBA).
+          </p>
+          <h2>Inloggen</h2>
+          <button onClick={login} className={styles.LoginBtn}>
+            Datapunt account
+          </button>
+          <p>
+            Je hebt hiervoor een account nodig. Heb je die niet? Heb je vragen
+            en/of suggesties?
+            <br />
+            <br /> Stuur dan een email naar{' '}
+            <a href="mailto:WBAkaart.V&OR@amsterdam.nl">
+              WBAkaart.V&OR@amsterdam.nl
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
