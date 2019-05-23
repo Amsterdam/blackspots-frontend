bb86eb7eae4f9c3cfe31a31176fe09e1187630c6
Bennie van der Wel <bennievanderwel@gmail.com>
2019-05-23 09:07:11 +0200

    WK-119 IE polyfill fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-app-polyfill/ie11';
+import '@babel/polyfill';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import auth from './shared/auth/auth';
