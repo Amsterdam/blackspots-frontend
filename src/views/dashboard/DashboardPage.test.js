e8ca676240af69d014cdcd52b567a51a0b314cd2
Bennie van der Wel <bennievanderwel@gmail.com>
2019-05-23 12:37:53 +0200

    Test clean up

--- src/views/dashboard/DashboardPage.test.js
+++ src/views/dashboard/DashboardPage.test.js
@@ -2,17 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import DashboardPage from './DashboardPage';
-import * as api from 'services/geo-api';
-import auth from 'shared/auth/auth';
+
 describe('DashboardPage', () => {
-  auth.default = jest.fn();
-  api.getAllBlackSpots = jest.fn();
-  delete window.Keycloak;
-  window.Keycloak = jest.fn();
   it('should render without errors', () => {
-    // jest.spyOn(api, 'getAllBlackSpots');
-    // jest.mock('services/geo-api.js');
-    // window = { Keycloak: () => {} };
     shallow(<DashboardPage />);
   });
 });
