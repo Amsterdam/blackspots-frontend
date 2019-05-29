81aef8d77cea587996a4a3802b15795d56ace85a
Bennie van der Wel <bennievanderwel@gmail.com>
2019-05-29 10:33:53 +0200

    WK-121 Altered the landing page intro text

--- src/views/landing/LandingPage.js
+++ src/views/landing/LandingPage.js
@@ -24,6 +24,17 @@ export default () => {
           <p className={styles.Intro}>
             Via deze pagina kun je inloggen op de kaart van de Werkgroep
             Blackspots Amsterdam (WBA).
+            <br />
+            <br /> De kaart bevat een overzicht van de Blackspots, Redroutes
+            (wegvakken), Protocol locaties (ernstig en dodelijk) en
+            Risicolocaties vanaf 2014 tot heden. Elke locatie op de kaart is
+            voorzien van een status vanaf onderzoek t/m uitvoering gereed of
+            geen maatregel.
+            <br />
+            <br /> De locaties zijn te filteren op startjaar behandeling WBA.
+            Indien van een locatie een definitieve rapportage en/of ontwerp
+            beschikbaar is, dan kun je die via de kaart downloaden. Elke locatie
+            kun je via de kaart op google maps bekijken.
           </p>
           <h2>Inloggen</h2>
           <button onClick={login} className={styles.LoginBtn}>
