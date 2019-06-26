7deea6ee3ea36bf3d6671e2227c1a2fb03405ea2
RM <RRMoelker@users.noreply.github.com>
2019-06-26 11:40:39 +0200

    Catch all Sentry configuration

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-app-polyfill/ie11';
 import '@babel/polyfill';
+import * as Sentry from '@sentry/browser';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import auth from './shared/auth/auth';
 import './styles/styles.scss';
 import './styles/fonts.scss';
+
+const environment = process.env.NODE_ENV;
+
+Sentry.init({
+  environment,
+  dsn: 'https://45be21450b804b1e85ad7462a529b0f8@sentry.data.amsterdam.nl/24',
+});
+
 
 class KeycloakWrapper extends React.Component {
   state = { authenticated: false };
