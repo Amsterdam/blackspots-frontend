ff33ea5201c4e9878477bc42ae9920e3377ae2fc
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-10-16 16:47:55 +0200

    added unittests for useKeycloak

---
+++ src/shared/hooks/useKeycloak.test.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useKeycloak from './useKeycloak';
+import auth from '../auth/auth';
+
+// jest.mock('../auth/auth');
+
+describe('useKeycloak', () => {
+  it('should return an object of positions with booleans when a component hit the viewport edge', () => {
+    const { result } = renderHook(() => useKeycloak());
+    const authenticated = result.current;
+    expect(authenticated).toEqual(false);
+    expect(auth.keycloak.onAuthSuccess).not.toBe(undefined);
+    expect(auth.keycloak.onAuthError).not.toBe(undefined);
+    expect(auth.keycloak.onAuthRefreshError).not.toBe(undefined);
+
+    act(() => {
+      auth.keycloak.onAuthSuccess();
+    });
+    expect(result.current).toEqual(true);
+
+    act(() => {
+      auth.keycloak.onAuthRefreshError();
+    });
+
+    expect(result.current).toEqual(false);
+
+    act(() => {
+      auth.keycloak.onAuthSuccess();
+    });
+    expect(result.current).toEqual(true);
+
+    act(() => {
+      auth.keycloak.onAuthRefreshError();
+    });
+
+    expect(result.current).toEqual(false);
+  });
+});
