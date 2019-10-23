478af314c96d475c774300db0d8a4ea2783938a7
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-10-23 13:19:09 +0200

    added unittests for the header

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { render, cleanup } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { UserContextProvider } from 'shared/user/UserContext';
 import Header from './Header';
 
+const history = createMemoryHistory();
+
 describe('Header', () => {
-  it('should render without errors', () => {
-    shallow(<Header />);
+  const regex = /toevoegen/i;
+  const withUserContext = (Component, user) => (
+    <UserContextProvider user={user}>
+      <Router history={history}>{Component}</Router>
+    </UserContextProvider>
+  );
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should not render the add link when the user has no add rights', () => {
+    var user = { canAdd: false };
+    const { container, queryByText } = render(
+      withUserContext(<Header />, user)
+    );
+    expect(queryByText(regex)).toBeNull();
+    expect(container.querySelectorAll('.Link')).toHaveLength(3);
+  });
+
+  it('should render the add link when the user has add rights', () => {
+    var user = { canAdd: true };
+    const { container, queryByText } = render(
+      withUserContext(<Header />, user)
+    );
+    expect(queryByText(regex)).not.toBeNull();
+    expect(container.querySelectorAll('.Link')).toHaveLength(4);
   });
 });
