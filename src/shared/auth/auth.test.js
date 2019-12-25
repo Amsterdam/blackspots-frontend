3d7d357b5cc0b0a258591af25a83f49160562bf2
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-12-25 08:20:21 +0100

    adds tests for auth service

--- src/shared/auth/auth.test.js
+++ src/shared/auth/auth.test.js
@@ -32,4 +32,24 @@ describe('auth', () => {
     await auth.userInfo();
     expect(keycloakMock.loadUserInfo).toHaveBeenCalled();
   });
+
+  it('should return the token', async () => {
+    keycloakMock.token = 'the-token';
+    const token = await auth.token();
+    expect(token).toEqual('Bearer the-token');
+  });
+
+  it('should check the roles', async () => {
+    keycloakMock.realmAccess.roles.push('bs_all');
+    const canAdd = await auth.canAdd();
+    expect(canAdd).toBeTruthy();
+    const canEdit = await auth.canEdit();
+    expect(canEdit).toBeTruthy();
+  });
+
+  it('should check the authentication', async () => {
+    keycloakMock.authenticated = true;
+    const isAuthenticated = await auth.isAuthenticated();
+    expect(isAuthenticated).toBeTruthy();
+  });
 });
