06a2d50fc75b1489cc8f6d48ba441cd9e3a1acfe
Jasper Swart <jasper.g.swart@gmail.com>
2020-12-08 10:18:13 +0100

    fixed detail

--- src/components/map/MapStyle.js
+++ src/components/map/MapStyle.js
@@ -2,13 +2,9 @@ import styled from 'styled-components';
 
 const MapStyle = styled.div`
   margin-top: 3px; /* allows the header border to be visible */
-  width: 100%;
+  width: 50%;
   height: 100%;
-
-  #mapdiv {
-    width: 100%;
-    height: 100%;
-  }
+  overflow: hidden;
 `;
 
 export default MapStyle;
