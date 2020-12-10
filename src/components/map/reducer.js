153f03e46f2e643868c35f174a04138eb0d1870e
Jasper Swart <jasper.g.swart@gmail.com>
2020-12-10 10:20:19 +0100

    added filter reducer

--- src/components/map/reducer.js
+++ src/components/map/reducer.js
@@ -1,41 +1,45 @@
+/* eslint-disable no-case-declarations */
+export const REDUCER_KEY = 'filter';
+export const SELECT_LOCATION = `${REDUCER_KEY}/SELECT_LOCATION`;
+export const ADD_LOCATIONS = `${REDUCER_KEY}/ADD_LOCATIONS`;
+export const SET_FILTER = `${REDUCER_KEY}/SET_FILTER`;
+
 export const initialState = {
   test: 'foo',
+  selectedLocation: null,
   locations: [],
+  filter: {},
 };
 
-export default (state, action) => {
+const filterReducer = (state = initialState, action) => {
   switch (action.type) {
-    // case RESET_LOCATION:
-    //   return {
-    //     ...state,
-    //     location: initialState.location,
-    //   };
-
-    // case SET_LOCATION:
-    //     ...state,
-    //     location: action.payload,
-    //   return {
-    //   };
-
-    // case SET_ADDRESS:
-    //   return {
-    //     ...state,
-    //     addressText: action.payload,
-    //   };
-
-    // case SET_VALUES:
-    //   return {
-    //     ...state,
-    //     ...action.payload,
-    //   };
-
-    // case SET_LOADING:
-    //   return {
-    //     ...state,
-    //     loading: action.payload,
-    //   };
-
+    case SELECT_LOCATION:
+      return {
+        ...state,
+        selectedLocation: { ...action.payload },
+      };
+    case ADD_LOCATIONS:
+      return {
+        ...state,
+        locations: [...action.payload],
+      };
+    case SET_FILTER:
+      return {
+        ...state,
+        filter: {
+          ...state,
+          ...action.payload,
+        },
+      };
     default:
       return state;
   }
 };
+
+export const actions = {
+  selectLocation: SELECT_LOCATION,
+  addLocations: ADD_LOCATIONS,
+  setFilter: SET_FILTER,
+};
+
+export default filterReducer;
