9d5832383a7e4bb504fdd0cbaed5ef92cadde76e
Jasper Swart <jasper.g.swart@gmail.com>
2020-12-14 11:07:40 +0100

    added GeoJSON to map part 1

--- src/components/map/hooks/useBlackspotsLayer.js
+++ src/components/map/hooks/useBlackspotsLayer.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useContext } from 'react';
+import React, { useRef, useEffect, useContext, useState } from 'react';
 import ReactDOM from 'react-dom';
 import L from 'leaflet';
 import { useMapInstance, GeoJSON } from '@amsterdam/react-maps';
@@ -8,81 +8,84 @@ import SVGIcon from '../../SVGIcon/SVGIcon';
 
 // import FilterContext from '../../../shared/reducers/FilterContext';
 
-export const handleFeature = (feature, layer, onClick) => {
-  layer.on('click', ({ latlng }) => {
-    onClick(feature, latlng);
-  });
-};
+// export const handleFeature = (feature, layer, onClick) => {
+//   layer.on('click', ({ latlng }) => {
+//     onClick(feature, latlng);
+//   });
+// };
 
-const createFeatureIcon = feature => {
-  // Leaflet only accepts HTML elements for custom markers so we need to
-  // create one from the SVGIcon
-  const { status, spot_type: spotType } = feature.properties;
-  const iconDiv = document.createElement('div');
-  ReactDOM.render(<SVGIcon type={spotType} status={status} />, iconDiv);
-  return {
-    // Add the correct classname based on type
-    // Risico types have a bigger icon therefore need more margin
-    className: `marker-div-icon ${
-      spotType === SpotTypes.RISICO ? 'large' : ''
-    } ${status === SpotStatusTypes.GEEN_MAATREGEL ? 'extra-opacity' : ''}`,
-    html: iconDiv,
-  };
-};
+// const createFeatureIcon = feature => {
+//   // Leaflet only accepts HTML elements for custom markers so we need to
+//   // create one from the SVGIcon
+//   const { status, spot_type: spotType } = feature.properties;
+//   const iconDiv = document.createElement('div');
+//   ReactDOM.render(<SVGIcon type={spotType} status={status} />, iconDiv);
+//   return {
+//     // Add the correct classname based on type
+//     // Risico types have a bigger icon therefore need more margin
+//     className: `marker-div-icon ${
+//       spotType === SpotTypes.RISICO ? 'large' : ''
+//     } ${status === SpotStatusTypes.GEEN_MAATREGEL ? 'extra-opacity' : ''}`,
+//     html: iconDiv,
+//   };
+// };
 
-export const createFeature = (feature, latlng) => {
-  // Create a marker with the correct icon and onClick method
-  return L.marker(latlng, {
-    icon: L.divIcon(createFeatureIcon(feature)),
-  });
-};
+// export const createFeature = (feature, latlng) => {
+//   // Create a marker with the correct icon and onClick method
+//   return L.marker(latlng, {
+//     icon: L.divIcon(createFeatureIcon(feature)),
+//   });
+// };
 
-const useBlackspotsLayer = (locations, onMarkerClick) => {
-  const mapInstance = useMapInstance();
-  const geoLayerRef = useRef(null);
-  useEffect(() => {
-    geoLayerRef.current = L.geoJSON(locations, {
-      // Add custom markers
-      onEachFeature(feature, layer) {
-        handleFeature(feature, layer, onMarkerClick);
-      },
-      pointToLayer: createFeature,
-    }).addTo(mapInstance);
-  }, [mapInstance, locations, onMarkerClick]);
+// const useBlackspotsLayer = (locations, onMarkerClick) => {
+//   const mapInstance = useMapInstance();
+//   const geoLayerRef = useRef(null);
+//   useEffect(() => {
+//     geoLayerRef.current = L.geoJSON(locations, {
+//       // Add custom markers
+//       onEachFeature(feature, layer) {
+//         handleFeature(feature, layer, onMarkerClick);
+//       },
+//       pointToLayer: createFeature,
+//     }).addTo(mapInstance);
+//   }, [mapInstance, locations, onMarkerClick]);
 
-  return geoLayerRef;
-};
+//   return geoLayerRef;
+// };
 
 const BlackspotsLayer = ({ onMarkerClick }) => {
   const { state } = useContext(FilterContext);
-  console.log('BlackspotsLayer', state.locations);
+  const [json, setJson] = useState('');
+  const mapInstance = useMapInstance();
+  const options = {};
 
-  // const mapInstance = useMapInstance();
-  // if (!mapInstance) {
-  //   return null;
-  // }
-  // const [json, setJson] = useState();
-  // // const geoLayerRef = useBlackspotsLayer(locations, onMarkerClick);
-  // const features = locations.reduce(
-  //   (acc, { layer: l }) => [...acc, ...l.features],
-  //   []
-  // );
-  // const layerData = {
-  //   type: 'FeatureCollection',
-  //   name: CATEGORY_NAMES.CAMERA_TOEZICHTSGEBIED,
-  //   crs: {
-  //     type: 'name',
-  //     properties: {
-  //       name: 'urn:ogc:def:crs:OGC:1.3:CRS84',
-  //     },
-  //   },
-  //   features,
-  // };
-  // setJson(layerData);
-  // layerName.current = layerData.name;
+  useEffect(() => {
+    if (mapInstance && state.locations.length) {
+      // const features = state.locations.reduce(
+      //   (acc, { layer: l }) => [...acc, ...l.features],
+      //   []
+      // );
 
-  // return locations ? <GeoJSON args={[json]} options={options} /> : null;
-  return null;
+      const features = [...state.locations];
+      const layerData = {
+        type: 'FeatureCollection',
+        name: 'foo',
+        crs: {
+          type: 'name',
+          properties: {
+            name: 'urn:ogc:def:crs:OGC:1.3:CRS84',
+          },
+        },
+        features,
+      };
+      console.log('BlackspotsLayer', layerData);
+
+      setJson(layerData);
+    }
+  }, [state.locations]);
+
+  return state.locations ? <GeoJSON args={[json]} options={options} /> : null;
+  // return null;
 };
 
 export default BlackspotsLayer;
