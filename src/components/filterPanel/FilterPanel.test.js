1b83ce66da6708f18d167b7b5052e62156ce050b
Jasper Swart <jasper.g.swart@gmail.com>
2021-01-21 13:09:31 +0100

    updated unit test for FilterPanel

--- src/components/filterPanel/FilterPanel.test.js
+++ src/components/filterPanel/FilterPanel.test.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { withTheme } from 'test/utils';
+import { FilterContext } from 'shared/reducers/FilterContext';
+import { initialState } from 'shared/reducers/filter';
 
 import FilterPanel from './FilterPanel';
 
-describe.skip('Filter Panel', () => {
-  const props = {
-    spotTypeFilter: {},
-    spotStatusTypeFilter: {},
-    blackspotYearFilter: {},
-    deliveredYearFilter: {},
-    quickscanYearFilter: {},
-    stadsdeelFilter: {},
-    setFilters: () => {},
-    setBlackspotListFilter: () => {},
-    setQuickscanListFilter: () => {},
-    setDeliveredListFilter: () => {},
-    setStadsdeelFilter: () => {},
-  };
+describe('FilterPanel', () => {
+  afterEach(cleanup);
 
-  it('should render without errors', () => {
-    shallow(<FilterPanel {...props} />);
+  it('should render correctly', () => {
+    const { queryByText, debug } = render(
+      withTheme(
+        <FilterContext.Provider
+          value={{ state: { filter: initialState.filter } }}
+        >
+          <FilterPanel />
+        </FilterContext.Provider>
+      )
+    );
+
+    debug();
+
+    expect(queryByText('Filters')).toBeInTheDocument();
+    expect(queryByText('Stadsdeel')).toBeInTheDocument();
+    // expect(queryByText('Noord')).toBeInTheDocument();
+    // expect(queryByText('Oost')).toBeInTheDocument();
+    // expect(queryByText('Centrum')).toBeInTheDocument();
+    // expect(queryByText('West')).toBeInTheDocument();
+    // expect(queryByText('Westpoort')).toBeInTheDocument();
+    // expect(queryByText('Zuid')).toBeInTheDocument();
+    // expect(queryByText('Zuidoost')).toBeInTheDocument();
   });
 });
