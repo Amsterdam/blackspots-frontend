3208bdcb13f1ef950b895365d9aeed6869ea8c4f
Jasper Swart <jasper.g.swart@gmail.com>
2021-01-21 15:50:58 +0100

    fixed unit test for  FilterPanel

--- src/components/filterPanel/FilterPanel.test.js
+++ src/components/filterPanel/FilterPanel.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { withTheme } from 'test/utils';
 import { FilterContext } from 'shared/reducers/FilterContext';
 import { initialState } from 'shared/reducers/filter';
@@ -9,8 +9,8 @@ import FilterPanel from './FilterPanel';
 describe('FilterPanel', () => {
   afterEach(cleanup);
 
-  it('should render correctly', () => {
-    const { queryByText, debug } = render(
+  it('should render ALL', () => {
+    const { queryByText } = render(
       withTheme(
         <FilterContext.Provider
           value={{ state: { filter: initialState.filter } }}
@@ -20,16 +20,66 @@ describe('FilterPanel', () => {
       )
     );
 
-    debug();
+    expect(queryByText('Filters')).toBeInTheDocument();
+    expect(queryByText('Type')).toBeInTheDocument();
+    expect(queryByText('Status')).toBeInTheDocument();
+    expect(queryByText('Stadsdeel')).toBeInTheDocument();
+  });
+
+  it('should render DELIVERED', () => {
+    const { queryByText, debug } = render(
+      withTheme(
+        <FilterContext.Provider
+          value={{
+            state: { filter: { ...initialState.filter, show: 'DELIVERED' } },
+          }}
+        >
+          <FilterPanel />
+        </FilterContext.Provider>
+      )
+    );
 
     expect(queryByText('Filters')).toBeInTheDocument();
+    expect(queryByText('Type')).toBeInTheDocument();
+    expect(queryByText('Status')).not.toBeInTheDocument();
     expect(queryByText('Stadsdeel')).toBeInTheDocument();
-    // expect(queryByText('Noord')).toBeInTheDocument();
-    // expect(queryByText('Oost')).toBeInTheDocument();
-    // expect(queryByText('Centrum')).toBeInTheDocument();
-    // expect(queryByText('West')).toBeInTheDocument();
-    // expect(queryByText('Westpoort')).toBeInTheDocument();
-    // expect(queryByText('Zuid')).toBeInTheDocument();
-    // expect(queryByText('Zuidoost')).toBeInTheDocument();
+  });
+
+  it('should render BLACKSPOTS', () => {
+    const { queryByText } = render(
+      withTheme(
+        <FilterContext.Provider
+          value={{
+            state: { filter: { ...initialState.filter, show: 'BLACKSPOTS' } },
+          }}
+        >
+          <FilterPanel />
+        </FilterContext.Provider>
+      )
+    );
+
+    expect(queryByText('Filters')).toBeInTheDocument();
+    expect(queryByText('Type')).not.toBeInTheDocument();
+    expect(queryByText('Status')).toBeInTheDocument();
+    expect(queryByText('Stadsdeel')).toBeInTheDocument();
+  });
+
+  it('should render QUICKSCANS', () => {
+    const { queryByText } = render(
+      withTheme(
+        <FilterContext.Provider
+          value={{
+            state: { filter: { ...initialState.filter, show: 'QUICKSCANS' } },
+          }}
+        >
+          <FilterPanel />
+        </FilterContext.Provider>
+      )
+    );
+
+    expect(queryByText('Filters')).toBeInTheDocument();
+    expect(queryByText('Type')).not.toBeInTheDocument();
+    expect(queryByText('Status')).toBeInTheDocument();
+    expect(queryByText('Stadsdeel')).toBeInTheDocument();
   });
 });
