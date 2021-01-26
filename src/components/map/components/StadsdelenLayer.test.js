4a05912d8549dcf36cb53b6b99a025dc41c1ca9c
Jasper Swart <jasper.g.swart@gmail.com>
2021-01-26 14:53:57 +0100

    added unit test for StadsdelenLayer

---
+++ src/components/map/components/StadsdelenLayer.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { withTheme } from 'test/utils';
+import { useMapInstance } from '@amsterdam/react-maps';
+import { mocked } from 'ts-jest';
+
+import StadsdelenLayer from './StadsdelenLayer';
+
+jest.mock('@amsterdam/react-maps');
+
+const mockedUseMapInstance = mocked(useMapInstance);
+
+describe('StadsdelenLayer', () => {
+  const addLayerSpy = jest.fn();
+
+  beforeEach(() => {
+    mockedUseMapInstance.mockImplementation(() => ({
+      addLayer: addLayerSpy,
+    }));
+  });
+
+  afterEach(cleanup);
+
+  it('enter search term and click on autosuggest item', () => {
+    render(withTheme(<StadsdelenLayer />));
+    expect(addLayerSpy).toHaveBeenCalledTimes(1);
+  });
+});
