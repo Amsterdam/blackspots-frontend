8a2fc2bd0d9da0bbad44d4f35d02714122d6f171
Jasper Swart <jasper.g.swart@gmail.com>
2021-02-09 12:34:00 +0100

    now using token in download links

--- src/components/detailPanel/components/DocumentLink.js
+++ src/components/detailPanel/components/DocumentLink.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
 import styled from 'styled-components';
 import { Icon, themeColor } from '@amsterdam/asc-ui';
 import { Download } from '@amsterdam/asc-assets';
 import { getWithToken } from 'shared/api/api';
 import auth from 'shared/auth/auth';
+import useDataFetching from 'shared/hooks/useDataFetching';
+import { compileFunction } from 'vm';
 import { ExternalLinkStyle } from '../DetailPanelStyle';
 import TextWithOverflow from './TextWithOverflow';
 
@@ -17,16 +19,43 @@ const DocumentLink = ({ document: documentData }) => {
   const trackDownload = () => {
     trackEvent({ category: 'PDF download', action: 'download' });
   };
+  const { results, fetchData } = useDataFetching('blob');
+  const url = `${documentData._links.self.href.split('?')[0]}file/`;
+  const { filename } = documentData;
+
+  useEffect(() => {
+    if (!results) return;
+
+    if (navigator.msSaveOrOpenBlob) {
+      navigator.msSaveOrOpenBlob(results, filename);
+    } else {
+      const href = global.URL.createObjectURL(results);
+      const link = document.createElement('a');
+      link.href = href;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+
+      global.URL.revokeObjectURL(href);
+      document.body.removeChild(link);
+    }
+  }, [results, filename]);
+
+  const handleDownload = useCallback(() => {
+    console.log('-----------------', documentData);
+    fetchData(url);
+  }, [fetchData, url]);
+
   return (
     <DocumentContainerStyle>
       <ExternalLinkStyle
         onClick={async e => {
           e.preventDefault();
           trackDownload();
-          const token = await auth.token();
+          handleDownload(e);
+
           // eslint-disable-next-line no-underscore-dangle
           const href = `${documentData._links.self.href.split('?')[0]}file/`;
-          getWithToken(href, null, null, token);
         }}
         download
         variant="inline"
