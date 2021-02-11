fb817cd91f0561000b72f0544176b341b82d54c4
Jasper Swart <jasper.g.swart@gmail.com>
2021-02-11 13:14:08 +0100

    updated landing page test

--- src/views/landing/LandingPage.test.js
+++ src/views/landing/LandingPage.test.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import { withTheme } from 'test/utils';
+import { render, fireEvent } from '@testing-library/react';
+import auth from 'shared/auth/auth';
 
 import LandingPage from './LandingPage';
 
+jest.mock('shared/auth/auth');
+
 describe('LandingPage', () => {
-  afterEach(cleanup);
-
   it('should render correctly', () => {
-    const { queryByText } = render(withTheme(<LandingPage />));
+    const { container, queryByTestId, getByText } = render(<LandingPage />);
 
-    expect(queryByText('Werkgroep Blackspots')).toBeInTheDocument();
-    expect(queryByText('Inloggen')).toBeInTheDocument();
+    // Header is renderd
+    expect(container.querySelector('div.Header')).toBeInTheDocument();
+
+    // More info link is rendered
+    expect(
+      container.querySelector(
+        'a[href="http://amsterdam.nl/verkeersveiligheid"]'
+      )
+    ).toBeInTheDocument();
+
+    const element = getByText('http://amsterdam.nl/verkeersveiligheid')
+      .parentNode;
+    expect(element.tagName).toEqual('P');
+
+    // Login button is renederd
+    expect(queryByTestId('login-button')).toBeInTheDocument();
+
+    // Account request instrictions link is rendered
+    expect(getByText('WBAkaart.V&OR@amsterdam.nl')).toBeInTheDocument();
+    expect(getByText('WBAkaart.V&OR@amsterdam.nl').tagName).toBe('A');
+    expect(
+      getByText('WBAkaart.V&OR@amsterdam.nl').getAttribute('href')
+    ).toEqual('mailto:WBAkaart.V&OR@amsterdam.nl');
+  });
+
+  it('should login on datapunt when Inloggen button is clicked', () => {
+    const loginMock = jest.spyOn(auth, 'login');
+    const { queryByText } = render(<LandingPage />);
+    expect(queryByText('Datapunt account')).toBeInTheDocument();
+    const button = queryByText('Datapunt account');
+    expect(button.getAttribute('type')).toEqual('button');
+
+    fireEvent.click(button);
+
+    expect(loginMock).toHaveBeenCalled();
   });
 });
