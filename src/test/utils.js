769a74cfecf29406b23e0704f82ea2806a7e8e24
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-20 18:55:09 +0100

    now using MemoryRouter for testing

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,5 +1,5 @@
 import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from '@amsterdam/asc-ui';
 import { UserContextProvider } from 'shared/user/UserContext';
 
@@ -7,16 +7,16 @@ export const history = createMemoryHistory();
 
 export const withUserContext = (Component, user) => {
   return (
-    <Router history={history}>
+    <MemoryRouter history={history}>
       <UserContextProvider user={user}>{Component}</UserContextProvider>;
-    </Router>
+    </MemoryRouter>
   );
 };
 
 export const withTheme = (Component) => {
   return (
     <ThemeProvider>
-      <Router history={history}>{Component}</Router>
+      <MemoryRouter history={history}>{Component}</MemoryRouter>
     </ThemeProvider>
   );
 };
