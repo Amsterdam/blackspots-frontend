e62dbfc4f87ab8446c20e9dfcc932910f730fb08
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-23 09:56:20 +0100

    added CustomRouter

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,21 +1,21 @@
 import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import CustomRouter from './CustomRouter';
 import { ThemeProvider } from '@amsterdam/asc-ui';
 import { UserContextProvider } from 'shared/user/UserContext';
 
 export const history = createMemoryHistory();
 export const withUserContext = (Component, user) => {
   return (
-    <Router history={history}>
+    <CustomRouter history={history}>
       <UserContextProvider user={user}>{Component}</UserContextProvider>;
-    </Router>
+    </CustomRouter>
   );
 };
 
 export const withTheme = (Component) => {
   return (
     <ThemeProvider>
-      <Router history={history}>{Component}</Router>
+      <CustomRouter history={history}>{Component}</CustomRouter>
     </ThemeProvider>
   );
 };
