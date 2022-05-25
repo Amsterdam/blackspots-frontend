ea5fe0125170583822103424aa7db654df41e6a7
janthijs <92784122+janthijs@users.noreply.github.com>
2022-05-25 09:36:37 +0200

    fix: remove deps

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 /* istanbul ignore file */
 import ReactDOM from 'react-dom';
-import 'regenerator-runtime/runtime';
+import { BrowserRouter } from 'react-router-dom';
 import * as Sentry from '@sentry/browser';
 import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './styles/styles.scss';
 import './styles/fonts.scss';
-
-// require('formdata-polyfill');
 
 const environment = process.env.NODE_ENV;
 
