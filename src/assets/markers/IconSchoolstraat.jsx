8761faa9d8b8fda91a4f923001233b57cd4114fc
janthijs <92784122+janthijs@users.noreply.github.com>
2022-06-02 16:04:35 +0200

    fix: update style and icon

--- src/assets/markers/IconSchoolstraat.jsx
+++ src/assets/markers/IconSchoolstraat.jsx
@@ -7,41 +7,28 @@ export default function IconSchoolstraat({ className, fill }) {
       height="28px"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
+      style={{ transform: 'rotate(45deg)' }}
       className={className}
       fill={fill}
     >
       <rect
         className="icon-status"
-        transform="rotate(225)"
-        x="-33.799"
-        y="-14"
+        transform="scale(-1)"
+        x="-28"
+        y="-28"
         width="28"
         height="28"
       />
-      <rect
-        transform="rotate(45)"
-        x="11.799"
-        y="-8"
-        width="16"
-        height="16"
-        fill="#fff"
-      />
-      <rect
-        transform="rotate(225)"
-        x="-26.883"
-        y="-7.0839"
-        width="14"
-        height="14"
-        fill="#000"
-      />
+      <rect x="6" y="6" width="16" height="16" fill="#fff" />
+      <rect x="7" y="7" width="14" height="14" fill="#000" />
       <text
-        transform="rotate(-3.4553)"
-        x="9.0327959"
-        y="19.092388"
+        transform="rotate(-45)"
+        x="-5.2436399"
+        y="24.098587"
         fill="#ffffff"
         xmlSpace="preserve"
       >
-        <tspan x="9.0327959" y="20.602388">
+        <tspan x="-3.9936399" y="24.098587">
           S
         </tspan>
       </text>
