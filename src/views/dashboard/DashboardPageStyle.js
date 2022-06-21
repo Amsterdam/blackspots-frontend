c7447b08ca52af1e75c2097e26a7ef340aabdbb2
janthijs <92784122+janthijs@users.noreply.github.com>
2022-06-21 13:42:29 +0200

    fix: update right style

--- src/views/dashboard/DashboardPageStyle.js
+++ src/views/dashboard/DashboardPageStyle.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 const DashboardPageStyle = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100%;
 `;
 
 export default DashboardPageStyle;
